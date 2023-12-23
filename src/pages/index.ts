1260958f5f523d2e8562e32106dae9c036f74b9c
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-12-23 23:06:19 +0100

    Create index.ts

---
+++ src/pages/index.ts
@@ -0,0 +1,4 @@
+export { default as Home } from "./Home";
+export { default as AboutMe } from "./AboutMe";
+export { default as Portfolio } from "./Portfolio";
+export { default as ContactMe } from "./ContactMe";
