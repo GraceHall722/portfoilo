4779c44ee4c6a6a0ffad25100fb6eb94e410e5a9
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2023-12-23 23:06:08 +0100

    Create index.tsx

---
+++ src/constants/index.tsx
@@ -0,0 +1,22 @@
+export const navlinks = [
+  {
+    id: "home",
+    title: "home",
+    link: "/",
+  },
+  {
+    id: "about-me",
+    title: "about-me",
+    link: "/about-me",
+  },
+  {
+    id: "projects",
+    title: "portfolio",
+    link: "/portfolio",
+  },
+  {
+    id: "contact-me",
+    title: "contacts",
+    link: "/contact-me",
+  },
+];
