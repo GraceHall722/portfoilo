5fd15b9a1fabdb65d9248ac879930b446a1b3c7d
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-01-04 18:53:37 +0100

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,7 @@
 export { default as Navbar } from "./Navbar";
 export { default as Footer } from "./Footer";
 export { default as FixedSideSocials } from "./FixedSideSocials";
+export { default as Quote } from "./Quote";
+export { default as HomeProject } from "./HomeProject";
+export { default as HomeSkill } from "./HomeSkill";
+export { default as HomeContact } from "./HomeContact";
