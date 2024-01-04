1ffc6ce93a1495405d6c5f6105bb5b40c5cd0daf
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-01-04 18:53:41 +0100

    Create Quote.tsx

---
+++ src/components/Quote.tsx
@@ -0,0 +1,20 @@
+const Quote = () => {
+  return (
+    <section className="mb-[15px] md:-mt-20 md:ml-[250px] mx-[10px] ">
+      <div className=" mx-[]">
+        <div className="text-[#64FFDB] ml-[10px] text-[60px] -mb-14 z-10 bg-black w">
+          "
+        </div>
+        <div className="font-fira-code text-[20px] text-white border md:w-8/12 pl-[15px] py-[20px]">
+          With great power comes great electricity bill!!
+        </div>
+
+        <div className="font-fira-code text-white text-[20px] border border-t-0 md1:w-3/12 lg:w-3/12 w-5/12">
+          <div className="py-[10px] pl-2">- Dr. Who</div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Quote;
