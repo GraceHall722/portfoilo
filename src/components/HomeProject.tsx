9359e91fc09b331a9d4e7619fdbc5160459f2b34
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-01-04 21:59:17 +0100

    Update HomeProject.tsx

--- src/components/HomeProject.tsx
+++ src/components/HomeProject.tsx
@@ -1,5 +1,13 @@
 const HomeProject = () => {
-  return <div>HomeProject</div>;
+  return (
+    <section className="mx-[150px] mt-[100px] flex flex-col md:flex-row">
+      <div className="font-fira-code text-white text-[32px] flex">
+        <div className="project">
+          <span className="text-[#64ffdb]">#</span>projects
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default HomeProject;
