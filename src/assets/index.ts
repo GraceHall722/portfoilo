52f2fdd0f98dc4f60fd241db53678af62d2dba07
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-01-04 17:50:18 +0100

    Update index.ts

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,5 +1,8 @@
 import Logo from "./Logo.svg";
 import Menu from "./Menu.svg";
 import closeMenu from "./closeMenu.svg";
+import Dots from "./Dots.svg";
+import Img1 from "./Img1.svg";
+import Image from "./Image.svg";
 
-export { Logo, Menu, closeMenu };
+export { Logo, Menu, closeMenu, Dots, Img1, Image };
