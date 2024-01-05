97f5c92cbbdddae711da3462a9ec8a6451c7e3c3
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-01-05 17:55:01 +0100

    adding assets

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -4,5 +4,8 @@ import closeMenu from "./closeMenu.svg";
 import Dots from "./Dots.svg";
 import Img1 from "./Img1.svg";
 import Image from "./Image.svg";
+import Photo1 from "./Photo1.svg";
+import Photo2 from "./Photo2.svg";
+import Photo3 from "./Photo3.svg";
 
-export { Logo, Menu, closeMenu, Dots, Img1, Image };
+export { Logo, Menu, closeMenu, Dots, Img1, Image, Photo1, Photo2, Photo3 };
