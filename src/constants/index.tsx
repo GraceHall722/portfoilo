73d249ab5224fe58eaff6ab5bdb8f51480d6082d
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-01-05 17:55:23 +0100

    Update index.tsx

--- src/constants/index.tsx
+++ src/constants/index.tsx
@@ -1,3 +1,5 @@
+import { Photo1, Photo2, Photo3 } from "../assets";
+
 export const navlinks = [
   {
     id: "home",
@@ -18,5 +20,32 @@ export const navlinks = [
     id: "contact-me",
     title: "contacts",
     link: "/contact-me",
+  },
+];
+
+export const homeProjects = [
+  {
+    id: 1,
+    image: Photo1,
+    skill: "HTML TailwindCss React",
+    title: "ChertNodes",
+    descr: "Minecraft server glitch Discord anti-crash bot",
+    link: "#",
+  },
+  {
+    id: 2,
+    image: Photo2,
+    skill: "HTML CSS React EthersJs Rust Solana",
+    title: "ProtectX",
+    descr: "Discord anti-crash bot Discord anti-crash bot",
+    link: "#",
+  },
+  {
+    id: 3,
+    image: Photo3,
+    skill: "HTML TailwindCss React Solidity NextJs",
+    title: "Kahoot Answers Viewer",
+    descr: "Get answers to your Kahoot quiz",
+    link: "#",
   },
 ];
