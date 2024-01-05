dc62f1e110f69c93dc324de19e8147992c72cf37
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-01-05 19:45:18 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { Home, AboutMe, Portfolio, ContactMe } from "./pages";
 
 import { Navbar, Footer } from "./components";
+import { Frame25, Frame28 } from "./assets";
 
 const App = () => {
   return (
@@ -15,7 +16,10 @@ const App = () => {
         <Route path="/contact-me" element={<ContactMe />} />
       </Routes>
 
-      {/* <HomeProject /> */}
+      <div className="relative">
+        <img src={Frame25} className="absolute bottom-10 right-0" />
+        <img src={Frame28} className="absolute md:bottom-80 bottom-96 left-0" />
+      </div>
 
       <Footer />
     </div>
