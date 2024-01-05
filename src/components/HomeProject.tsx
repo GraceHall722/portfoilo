59bee595741724e49ac5e5c86d8b7eac2eb4b0a0
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-01-05 19:45:35 +0100

    Update HomeProject.tsx

--- src/components/HomeProject.tsx
+++ src/components/HomeProject.tsx
@@ -24,7 +24,7 @@ const HomeProject = () => {
   }, []);
 
   return (
-    <div>
+    <div className="flex flex-wrap">
       <section className="md:mx-[150px] mx-[30px] mt-[100px] flex flex-col mb-[15px] md:flex-row">
         <div className="font-fira-code text-white text-[32px] flex">
           <div className="flex flex-col md:flex-row">
@@ -34,7 +34,7 @@ const HomeProject = () => {
             <div className="md1:w-96 md:w-48 h-1 ml-2 mt-[25px] bg-[#FFBF00]"></div>
             <div className="text-[15px] flex mt-[18px] ml-[150px] md:ml-[50px] xl:ml-[150px]">
               <Link to="/portfolio">
-                <div className="md:flex-row flex">
+                <div className="md:flex-row flex transform transition duration-500 ease-in-out hover:scale-110">
                   More <span className="ml-2">~~~{">"}</span>
                 </div>
               </Link>
@@ -47,7 +47,7 @@ const HomeProject = () => {
         {displayProjects.map((project, index) => (
           <div
             key={project.id}
-            className={`border mt-[20px] w-9/12 ${
+            className={`border mt-[20px] w-9/12 transform transition duration-500 ease-in-out hover:scale-95 ${
               index === 0 ? "ml-[10px]" : ""
             }`}
           >
@@ -58,7 +58,7 @@ const HomeProject = () => {
             <h2 className="text-white m-[20px] text-[25px]">{project.title}</h2>
             <p className="text-[#939ba8] mx-[20px]">{project.descr}</p>
             <Link to={project.link}>
-              <button className="border border-[#64FFDB] text-white text-[18px] font-bold m-[20px] p-[10px] transform transition duration-500 ease-in-out hover:scale-110">
+              <button className="border border-[#64FFDB] text-white text-[18px] font-bold m-[20px] p-[10px] transform transition duration-500 ease-in-out hover:scale-110 hover:border-[#FFBF00]">
                 View Project
               </button>{" "}
             </Link>
