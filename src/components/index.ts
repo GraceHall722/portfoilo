faf997147f7613600c50a94172b427f5e0acc783
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-01-09 14:07:35 +0100

    Update index.ts

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,6 +1,9 @@
 export { default as Navbar } from "./Navbar";
-export { default as Footer } from "./Footer";
 export { default as FixedSideSocials } from "./FixedSideSocials";
 export { default as Quote } from "./Quote";
 export { default as HomeProject } from "./HomeProject";
 export { default as Experience } from "./Experience";
+export { default as Experience1 } from "./work/Experience1";
+export { default as Experience2 } from "./work/Experience2";
+export { default as Experience3 } from "./work/Experience3";
+export { default as Footer } from "./Footer";
