6242a3ad729307148b680d47624ea1700dde84e8
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-01-09 14:07:31 +0100

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,97 +1,55 @@
-import { useState } from "react";
-import { RiGithubFill, RiLinkedinFill } from "react-icons/ri";
-import { SiSubstack } from "react-icons/si";
-import { Link, NavLink } from "react-router-dom";
-
-import { Logo } from "../assets";
+import { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
 
 const Footer = () => {
-  const [activeLink, setActiveLink] = useState(location.pathname);
-  const [isHoveredGithub, setIsHoveredGithub] = useState(false);
-  const [isHoveredLinkedin, setIsHoveredLinkedin] = useState(false);
-  const [isHoveredSubstack, setIsHoveredSubstack] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    if (pathname === "/contact-me") {
+      window.scrollTo(0, 0);
+    }
+  }, [pathname]);
 
   return (
-    <footer className="md:flex-row flex-col font-fira-code ">
-      <div className="flex flex-col md:flex-row border-t border-[#939BA8] w-11/12 ml-4 md:ml-[50px]">
-        <NavLink to="/" onClick={() => setActiveLink("/")}>
-          <div className="flex flex-col md:flex-row ml-4 md:ml-10 md1:ml-[150px] items-center justify-between mt-10 md:mt-[50px]">
-            <div className="flex flex-col mb-2">
-              <div className="flex mb-2">
-                <img
-                  src={Logo}
-                  className="w-[20px] h-[30px] flex"
-                  style={{
-                    filter:
-                      "brightness(0) saturate(100%) invert(98%) sepia(93%) saturate(841%) hue-rotate(75deg) brightness(105%) contrast(123%)",
-                  }}
-                />
-                <h3 className="text-white text-base text-bold mx-3 flex">
-                  Patrick
-                </h3>
-              </div>
-              <p className="text-[#939BA8] flex-col">
-                osepatrick.ehimen77@gmail.com
-              </p>
-            </div>
-          </div>
-          <div>
-            <p className="ss:ml-[65px] ssm:ml-[150px] md:ml-[43px] md1:ml-[150px] text-[#939BA8] my-2 md:my-[10px]">
-              Blockchanin Engineer{" "}
-            </p>
-          </div>
-        </NavLink>
+    <section className="justify-center flex items-center mt-[100px] font-fira-code flex-col">
+      <div className="text-[#64FFDB] text-[20px]">
+        <span className="text-[18px]">#</span>What's Next?
+      </div>
+      <div className="text-[#CCD6F6] md:text-[50px] text-[30px] text-bold">
+        Get In Touch
+      </div>
+      <div className="text-[#636E8A] text-center mx-[40px]">
+        Although I'm Always open for any new opportunities,{" "}
+        <br className="hidden md:flex" />
+        my inbox is open. Whether you have a question or just want to say hi,{" "}
+        <br className="hidden md:flex" />
+        I'll try my best to get back to you!
+      </div>
+      <button className="my-[50px] border text-[#64ffdb] border-[#64ffdb] rounded-md transform transition duration-500 ease-in-out hover:scale-110">
+        <Link to="/contact-me">
+          <div className="text-[18px] mx-[15px] my-[7px]">Contact Me</div>
+        </Link>
+      </button>
 
-        <div className="flex flex-col md1:flex-row mt-[50px] ss:ml-[65px] ssm:ml-[150px] ml-[100px] md:ml-[35px] lg:mx-[150px]">
-          <p className="flex text-[#939BA8] pb-2 md:pb-[10px]">
-            You can find me here:
-          </p>
-          <div className="flex px-[10px]">
-            <Link
-              target="_blank"
-              to={"https://github.com/Patrick-Ehimen"}
-              className="px-[10px] transform hover:scale-110"
-              onMouseEnter={() => setIsHoveredGithub(true)}
-              onMouseLeave={() => setIsHoveredGithub(false)}
-            >
-              <RiGithubFill
-                size={24}
-                color={isHoveredGithub ? "#64FFDB" : "#939BA8"}
-              />
-            </Link>
-            <Link
-              target="_blank"
-              to={"https://www.linkedin.com/in/patrick-ehimen/"}
-              className="px-[10px] transform hover:scale-110"
-              onMouseEnter={() => setIsHoveredLinkedin(true)}
-              onMouseLeave={() => setIsHoveredLinkedin(false)}
-            >
-              <RiLinkedinFill
-                size={24}
-                color={isHoveredLinkedin ? "#64FFDB" : "#939BA8"}
-              />
-            </Link>
-            <Link
-              target="_blank"
-              to={"#"}
-              className="px-[10px] transform hover:scale-110"
-              onMouseEnter={() => setIsHoveredSubstack(true)}
-              onMouseLeave={() => setIsHoveredSubstack(false)}
-            >
-              <SiSubstack
-                size={24}
-                color={isHoveredSubstack ? "#64FFDB" : "#939BA8"}
-              />
-            </Link>
+      <div
+        className="z-10 fixed bottom-0 md1:hidden right-0 hidden md:flex flex-row px-12 items-center justify-between"
+        style={{ transform: "translateY(0%) translateZ(0px)" }}
+      >
+        <div className="flex flex-col space-y-8 items-center justify-center">
+          <div className="flex flex-col space-y-3 items-center justify-center">
+            <a href="src/constants/0x0se.pdf" download>
+              <div
+                style={{ writingMode: "vertical-rl" }}
+                className="font-fira-code text-[20px] text-[#CCD6F6] hover:text-[#64ffdb] transform transition duration-500 ease-in-out hover:scale-110"
+              >
+                My Resume
+              </div>
+            </a>
+            <div className="h-36 w-0.5 bg-gray-400 mt-0"></div>
           </div>
         </div>
       </div>
-      <div className="mx-4 md1:mx-[150px] md:ml-[30px] p-4 md1:w-9/12 text-[#939BA8]">
-        <p className=" pl-4 lg:pl-[50px] xl:pl-[100px] pt-4 md:pt-[20px] mt-4 md:mt-[20px] border-t border-[#939BA8] text-center md:mx-[50px] lg:text-justify">
-          Copyright © 2024 - All right reserved by Patrick-Ehimen
-        </p>
-      </div>
-    </footer>
+    </section>
   );
 };
 
