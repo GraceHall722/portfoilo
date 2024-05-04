0b4453ddd70e9d63cfffb2c3644710616c98d9e8
Patrick-Ehimen <osepatrick.ehimen77@gmail.com>
2024-05-04 22:55:49 +0100

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -58,7 +58,7 @@ const Navbar = () => {
             </li>
           ))}
           <button className="border hidden md1:flex border-[#64FFDB] w-auto transform transition duration-500 ease-in-out hover:scale-110">
-            <a href="src/constants/0xOse.pdf" download>
+            <a href="src/constants/0xOse.pdf" target="blank">
               <div className="mx-[20px] text-white">Resume</div>
             </a>
           </button>
